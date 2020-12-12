f8caeec2af04e844f79a5ee12d966cd3b95c644a
GL <2691510552@qq.com>
2020-12-12 10:23:10 +0800

    admin

---
+++ ex.py
@@ -0,0 +1,5 @@
+test_data=[1,"kevin","abc","hello"]
+test_data.append("cc")
+test_data.insert(1,"bb")
+
+print(test_data)
---
+++ ex2.py
@@ -0,0 +1,41 @@
+# # # # test_data={
+# # # #
+# # # #     'name':"王盼",
+# # # #     'age':20
+# # # # }
+# # # # test_data.update({
+# # # #
+# # # #     'name':"张芳",
+# # # #     'age':20,
+# # # #     'xingbie':"男"
+# # # #
+# # # # })
+# # # # print(test_data)
+# # # # test_data="abc"
+# # # # x,y,z=test_data
+# # # # print(y)#解包
+# # #
+# # # #封包
+# # # # x=1,5,
+# # # # print(x)
+# # # '''把3和“work”连起来'''
+# # # x=3
+# # # print(str(x)+"work")
+# # # y="3.14"
+# # # print(float(y)+1)
+# # #
+# # # test_data="wei,王盼"
+# # # print("。" in test_data)
+# # #
+# # # x=[1,2,3]
+# # # x1=[1,2,3]
+# # # print(x==x1)
+# # x=2
+# # y=2
+# # assert x != y ,"测试用例不通过"
+# # print("finish")
+# test_data=[1,2,3,5,7,8,9,]
+# for i in test_data:
+#     if i==3:
+#         break#终止
+#     print(i)
